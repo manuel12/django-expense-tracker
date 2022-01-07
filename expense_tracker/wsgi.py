38acf9f4c129402cd9e18733807201931a6bf71f
Manuel <manuelpinedacabeza@gmail.com>
2022-01-07 18:48:51 +0100

    Removed required meta tags comment. Removed bootstrap comment. Updated title from Expense Tracker App to Expense Tracker. Updated pagination functions.

--- expense_tracker/wsgi.py
+++ expense_tracker/wsgi.py
@@ -7,7 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-
 from django.core.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
