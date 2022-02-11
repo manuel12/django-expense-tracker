827b17c3b3037a8d64908bc585473d74524bb321
Manuel <manuelpinedacabeza@gmail.com>
2022-02-11 21:58:30 +0100

    Added default settings and added Cling.

--- expense_tracker/wsgi.py
+++ expense_tracker/wsgi.py
@@ -7,5 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+import os
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expense_tracker.settings")
+application = Cling(get_wsgi_application())
