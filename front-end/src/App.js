b30731008aa235400461a7cb4b26b9fdbe04e760
Manuel <manuelpinedacabeza@gmail.com>
2023-07-13 11:25:51 +0200

    Imported react, routes-related stuff and Home and Charts pages. Implemented routes.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Charts from "./pages/Charts/Charts";
 
 function App() {
+  const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
+  const [username, setUsername] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div className='App'>
+        {" "}
+        <nav id='navbar' className='navbar justify-content-center'>
+          <a className='navbar-brand navbar-link font-weight-bold' href='/home'>
+            Expense Tracker
+          </a>
+
+          <ul className='navbar-nav navbar-list flex-row justify-content-around'>
+            <li className='nav-item active'>
+              <Link className='nav-link' to='/home'>
+                Home <span className='sr-only'>(current)</span>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/charts'>
+                Charts & Statistics
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div
+          id='user-greet'
+          className='user-greet font-weight-bold'
+          data-test='user-greet'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* {% if user.is_authenticated %} */}
+          <p>
+            Hi
+            {/* {{ user.username }} */}! |
+            <a href="{% url 'accounts:logout' %}" data-test='logout-link'>
+              Log Out
+            </a>
+          </p>
+          {/* {% else %} */}
+          <p>You are not logged in.</p>
+          <a href="{% url 'accounts:signup' %}">Sign Up</a> |
+          <a href="{% url 'accounts:login' %}" data-test='logout'>
+            Log In
+          </a>
+          {/* {% endif %} */}
+        </div>
+        <div className='container' data-test='container'>
+          {/* {% block content %} {% endblock content %} */}
+        </div>
+      </div>
+
+      <Routes>
+        <Route exact path='/home' element={<Home />} />
+        <Route path='/charts' element={<Charts />} />
+      </Routes>
+    </Router>
   );
 }
 
