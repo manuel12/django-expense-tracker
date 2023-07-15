2063968135d89d895520c0292b26c7e8ec274951
Manuel <manuelpinedacabeza@gmail.com>
2023-07-15 13:51:03 +0200

    Imported and used UserGreet component. Wrapped routes around container component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Charts from "./pages/Charts/Charts";
 
+import UserGreet from "./components/UserGreet/UserGreet";
+
 function App() {
   const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
   const [username, setUsername] = useState("");
@@ -31,36 +33,15 @@ function App() {
             </li>
           </ul>
         </nav>
-        <div
-          id='user-greet'
-          className='user-greet font-weight-bold'
-          data-test='user-greet'
-        >
-          {/* {% if user.is_authenticated %} */}
-          <p>
-            Hi
-            {/* {{ user.username }} */}! |
-            <a href="{% url 'accounts:logout' %}" data-test='logout-link'>
-              Log Out
-            </a>
-          </p>
-          {/* {% else %} */}
-          <p>You are not logged in.</p>
-          <a href="{% url 'accounts:signup' %}">Sign Up</a> |
-          <a href="{% url 'accounts:login' %}" data-test='logout'>
-            Log In
-          </a>
-          {/* {% endif %} */}
-        </div>
+        <UserGreet />
+        {/* {% block content %} {% endblock content %} */}
         <div className='container' data-test='container'>
-          {/* {% block content %} {% endblock content %} */}
+          <Routes>
+            <Route exact path='/home' element={<Home />} />
+            <Route path='/charts' element={<Charts />} />
+          </Routes>
         </div>
       </div>
-
-      <Routes>
-        <Route exact path='/home' element={<Home />} />
-        <Route path='/charts' element={<Charts />} />
-      </Routes>
     </Router>
   );
 }
