bed1d83afd088939ffe16518803a2f5ccf5f50cf
Manuel <manuelpinedacabeza@gmail.com>
2023-07-16 14:12:13 +0200

    Removed hardcoded navbar, imported and used Navbar component instead. Changed home route to /

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Charts from "./pages/Charts/Charts";
 
+// import Navbar from "./components/Navbar/Navbar";
 import UserGreet from "./components/UserGreet/UserGreet";
 
 function App() {
@@ -15,29 +16,12 @@ function App() {
     <Router>
       <div className='App'>
         {" "}
-        <nav id='navbar' className='navbar justify-content-center'>
-          <a className='navbar-brand navbar-link font-weight-bold' href='/home'>
-            Expense Tracker
-          </a>
-
-          <ul className='navbar-nav navbar-list flex-row justify-content-around'>
-            <li className='nav-item active'>
-              <Link className='nav-link' to='/home'>
-                Home <span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link' to='/charts'>
-                Charts & Statistics
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        {/* <Navbar /> */}
         <UserGreet />
         {/* {% block content %} {% endblock content %} */}
         <div className='container' data-test='container'>
           <Routes>
-            <Route exact path='/home' element={<Home />} />
+            <Route exact path='/' element={<Home />} />
             <Route path='/charts' element={<Charts />} />
           </Routes>
         </div>
