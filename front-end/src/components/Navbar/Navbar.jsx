e8d369f98fcebcffda471c9cd4bef26f7a357f34
Manuel <manuelpinedacabeza@gmail.com>
2023-07-17 21:30:19 +0200

    Renamed Link to RouterLink.

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
 import "./styles.css";
 
 import React from "react";
-// import { BrowserRouter as Link } from "react-router-dom";
 
-const Link = () => <></>;
-
-const Navbar = () => {
+const Navbar = ({ RouterLink }) => {
   return (
     <nav id='navbar' className='navbar justify-content-center'>
-      <a className='navbar-brand navbar-link font-weight-bold' href='/home'>
+      <a
+        className='navbar-brand navbar-routerLink font-weight-bold'
+        href='/home'
+      >
         Expense Tracker
       </a>
 
       <ul className='navbar-nav navbar-list flex-row justify-content-around'>
         <li className='nav-item active'>
-          <Link className='nav-link' to='/home'>
+          <RouterLink className='nav-link' to='/'>
             Home <span className='sr-only'>(current)</span>
-          </Link>
+          </RouterLink>
         </li>
         <li className='nav-item'>
-          <Link className='nav-link' to='/charts'>
+          <RouterLink className='nav-link' to='/charts'>
             Charts & Statistics
-          </Link>
+          </RouterLink>
         </li>
       </ul>
     </nav>
