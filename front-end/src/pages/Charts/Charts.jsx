eee13084126ae3ae775f9d4b81a7f641619d2d99
Manuel <manuelpinedacabeza@gmail.com>
2023-07-18 15:31:21 +0200

    Removed hardcoded StatisticsTable and imported and used as a component instead.

--- front-end/src/pages/Charts/Charts.jsx
+++ front-end/src/pages/Charts/Charts.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 
 import BarChart from "../../charts/BarChart/BarChart";
 import PieChart from "../../charts/PieChart/PieChart";
+import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
 
 const Charts = () => {
   const [expenses, setExpenses] = useState([1, 2, 3]);
@@ -88,123 +89,7 @@ const Charts = () => {
         </>
       )}
 
-
-      {/* {% endif %}  */}
-
-      {/* {% if expenses %} */}
-      {/* <div id="statistics-table" className="statistics-table" data-test="statistics-table">
-    <div className="row">
-      <div className="col-md-12">
-        <table className="w-100 table-striped table-hover table-bg">
-          <tbody>
-            <tr>
-              <td>Current month's total expenses:</td>
-              <td className="statistics" data-test="stats-current-month-expenses">
-                € {{ statistics.curr_month_expense_sum }} {% if budget %} / € {{ budget }}
-
-                {% if budget > statistics.curr_month_expense_sum %}
-                <span className="badge badge-pill badge-success"> under budget </span>
-                {% else %}
-                <span className="badge badge-pill badge-danger"> over budget </span>
-                {% endif %}
-                <a
-                  href="{% url 'expenses:update_budget' %}"
-                  className="font-weight-bold"
-                  data-test="update-budget"
-                >
-                  <span className="badge-pill badge-warning">✎</span>
-                </a>
-                <a
-                  href="{% url 'expenses:delete_budget' %}"
-                  className="font-weight-bold"
-                  data-test="delete-budget"
-                >
-                  <span className="badge-pill badge-danger">X</span>
-                </a>
-                {% endif %}
-              </td>
-            </tr>
-            <tr>
-              <td>Last month's total expenses:</td>
-              <td className="statistics" data-test="stats-last-month-expenses">
-                € {{ statistics.one_month_ago_expense_sum }}
-              </td>
-            </tr>
-            <tr>
-              <td>Current Month vs Last Month's (%) comparison:</td>
-              <td
-                className="statistics"
-                data-test="stats-current-month-vs-last-month-comparison"
-              >
-                {% if statistics.monthly_percentage_diff == 0 %}
-                <span> 0% </span>
-                {% elif statistics.monthly_percentage_diff > 100 %}
-                <span className="badge badge-pill badge-danger">
-                  {{ statistics.monthly_percentage_diff }}%
-                </span>
-                {% else%}
-                <span className="badge badge-pill badge-success">
-                  {{ statistics.monthly_percentage_diff }}%
-                </span>
-                {% endif %}
-              </td>
-            </tr>
-            <tr>
-              <td>Monthly expense average:</td>
-              <td className="statistics" data-test="stats-monthly-expense-average">
-                € {{ statistics.monthly_expense_average }}
-              </td>
-            </tr>
-
-            <tr>
-              <td>Daily expense average:</td>
-              <td className="statistics" data-test="stats-daily-expense-average">
-                € {{ statistics.daily_expense_average }}
-              </td>
-            </tr>
-            <tr>
-              <td>Biggest category expenditure:</td>
-              <td className="statistics" data-test="stats-biggest-category-expense">
-                {{ statistics.biggest_category_expenditure.category }}:
-                <p className="black-font">
-                  € {{ statistics.biggest_category_expenditure.amount }}
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>Smallest category expenditure:</td>
-              <td className="statistics" data-test="stats-smallest-category-expense">
-                {{ statistics.smallest_category_expenditure.category }}:
-                <p className="black-font">
-                  € {{ statistics.smallest_category_expenditure.amount }}
-                </p>
-              </td>
-            </tr>
-            <tr>
-              <td>Biggest expense:</td>
-              <td className="statistics" data-test="stats-biggest-expense">
-                ({{ statistics.max_expense_content}})
-                <p className="black-font">€ {{ statistics.max_expense }}</p>
-              </td>
-            </tr>
-            <tr>
-              <td>Smallest expense:</td>
-              <td className="statistics" data-test="stats-smallest-expense">
-                ({{ statistics.min_expense_content}})
-                <p className="black-font">€ {{ statistics.min_expense }}</p>
-              </td>
-            </tr>
-            <tr className="font-weight-bold">
-              <td>Total expenses:</td>
-              <td className="statistics" data-test="stats-total-expenses">
-                € {{ statistics.sum_expense }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div> */}
+      {expenses && <StatisticsTable />}
     </>
   );
 };
