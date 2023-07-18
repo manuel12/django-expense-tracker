75d285ffc0a9d62b09a99f9e5421afa90c4afbc7
Manuel <manuelpinedacabeza@gmail.com>
2023-07-18 15:32:55 +0200

    Added CustomForm component

---
+++ front-end/src/components/CustomForm/CustomForm.jsx
@@ -0,0 +1,53 @@
+import "./styles.css";
+
+import React from "react";
+
+const CustomForm = ({
+  title,
+  formFields = [],
+  formText = "",
+  dataTestIdForm = "",
+  submitBtnText = "Save",
+  submitBtnColor = "primary",
+  dataTestIdSubmitBtn = "",
+  cancelBtn = false,
+  dataTestIdCancelBtn = "",
+}) => {
+  return (
+    <div className='form-group'>
+      <h3>{title}</h3>
+      <form
+        className='font-weight-bold'
+        method='post'
+        data-test={dataTestIdForm}
+      >
+        {/* {% csrf_token %} {{ form.as_p }} */}
+        {formFields.map((field) => (
+          <p>
+            <label>{field}:</label>
+            <input type='text' name={field} className='form-control'></input>
+          </p>
+        ))}
+        {formText && <p>{formText}</p>}
+        <button
+          type='submit'
+          className={`btn btn-${submitBtnColor}`}
+          data-test={dataTestIdSubmitBtn}
+        >
+          {submitBtnText}
+        </button>
+        {cancelBtn && (
+          <a
+            className='btn btn-secondary cancel-btn'
+            href="{% url 'expenses:home' %}"
+            data-test={dataTestIdCancelBtn}
+          >
+            Cancel
+          </a>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default CustomForm;
