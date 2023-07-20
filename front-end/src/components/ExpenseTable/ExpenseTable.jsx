16dd8c3654ddbab78f80b23bbda5c260b5ee349e
Manuel <manuelpinedacabeza@gmail.com>
2023-07-20 21:52:32 +0200

    Removed hardocded expense data, used fecth instead.

--- front-end/src/components/ExpenseTable/ExpenseTable.jsx
+++ front-end/src/components/ExpenseTable/ExpenseTable.jsx
@@ -1,45 +1,34 @@
 import "./styles.css";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const ExpenseTable = () => {
-  const expenseTableData = [
-    {
-      amount: 25.0,
-      content: "Gym memebership",
-      category: "Monthly bill",
-      source: "McFit Gym",
-      date: "2023-07-17 00:00:00",
-    },
-    {
-      amount: 9.99,
-      content: "Audible bill",
-      category: "Monthly bill",
-      source: "Audible",
-      date: "2023-07-17 00:00:00",
-    },
-    {
-      amount: 18.0,
-      content: "avocado, salmon, blueberries",
-      category: "Groceries",
-      source: "Carrefour",
-      date: "2023-07-17 00:00:00",
-    },
-    {
-      amount: 7.0,
-      content: "Uber",
-      category: "Taxi fare",
-      source: "Uber",
-      date: "2023-07-17 00:00:00",
-    },
-    {
-      amount: 6.0,
-      content: "HDMI Cable",
-      category: "Online shopping",
-      source: "Amazon",
-      date: "2023-07-17 00:00:00",
-    },
-  ];
+const ExpenseTable = ({expenses}) => {
+  const [accessToken, setAccessToken] = useState(
+    JSON.parse(localStorage.getItem("accessToken"))
+  );
+
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const fetchExpenses = async () => {
+    const res = await fetch("http://localhost:8000/api/expenses/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (res.ok) {
+      const paginatedExpenses = await res.json();
+      console.log(paginatedExpenses);
+      // setExpenses(paginatedExpenses.results);
+    } else {
+      throw new Error("Fetching expenses failed");
+    }
+  };
 
   return (
     <div id='expense-table' className='expense-table' data-test='expense-table'>
@@ -57,33 +46,34 @@ const ExpenseTable = () => {
         </thead>
 
         <tbody data-test='expense-table-body'>
-          {expenseTableData.map((expense) => {
-            return (
-              <tr>
-                <td>{expense.date}</td>
-                <td>{expense.source}</td>
-                <td>{expense.category}</td>
-                <td>{expense.content}</td>
-                <td>€ {expense.amount}</td>
-                <td className='font-weight-bold'>
-                  <a
-                    href="{% url 'expenses:update' expense.pk %}"
-                    data-test='update-expense-{{ expense.pk }}'
-                  >
-                    <span className='badge-pill badge-warning'>✎</span>
-                  </a>
-                </td>
-                <td className='font-weight-bold'>
-                  <a
-                    href="{% url 'expenses:delete' expense.pk %}"
-                    data-test='delete-expense-{{ expense.pk }}'
-                  >
-                    <span className='badge-pill badge-danger'>X</span>
-                  </a>
-                </td>
-              </tr>
-            );
-          })}
+          {expenses.length > 0 &&
+            expenses.map((expense, i) => {
+              return (
+                <tr key={i}>
+                  <td>{expense.date}</td>
+                  <td>{expense.source}</td>
+                  <td>{expense.category}</td>
+                  <td>{expense.content}</td>
+                  <td>€ {expense.amount}</td>
+                  <td className='font-weight-bold'>
+                    <a
+                      href='/update'
+                      data-test='update-expense-{{ expense.pk }}'
+                    >
+                      <span className='badge-pill badge-warning'>✎</span>
+                    </a>
+                  </td>
+                  <td className='font-weight-bold'>
+                    <a
+                      href='expenses/delete/'
+                      data-test='delete-expense-{{ expense.pk }}'
+                    >
+                      <span className='badge-pill badge-danger'>X</span>
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
