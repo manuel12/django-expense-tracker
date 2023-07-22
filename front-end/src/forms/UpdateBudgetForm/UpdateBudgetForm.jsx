5a7a8987cfbc6d8365084f92c0d0719d855a10e3
Manuel <manuelpinedacabeza@gmail.com>
2023-07-22 15:10:42 +0200

    Imported states, useParams and CustomForm.
    Added request to fetch budget data.
    
    Added request to update budget data.

--- front-end/src/forms/UpdateBudgetForm/UpdateBudgetForm.jsx
+++ front-end/src/forms/UpdateBudgetForm/UpdateBudgetForm.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import CustomForm from "../../components/CustomForm/CustomForm";
 
 const UpdateBudgetForm = () => {
   const [accessToken, setAccessToken] = useState(
     JSON.parse(localStorage.getItem("accessToken"))
   );
+  const { id } = useParams();
   const [amount, setAmount] = useState(0);
+
+  useEffect(() => {
+    fetchBudgetData();
+  }, []);
+
+  const fetchBudgetData = async () => {
+    const res = await fetch("http://localhost:8000/api/budget/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (res.ok) {
+      const budgetData = await res.json();
+      console.log(budgetData);
+      setAmount(budgetData.amount);
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-1
-    const res = await fetch("http://localhost:8000/api/budget/update/", {
-      method: "POST",
+
+    const res = await fetch(`http://localhost:8000/api/budget/update/${id}/`, {
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${accessToken}`,
@@ -18,12 +42,12 @@ const UpdateBudgetForm = () => {
       body: JSON.stringify({ amount }),
     });
 
-    if (res.status === 201) {
-      console.log("Creating budget successful!");
+    if (res.status === 200) {
+      console.log("Updating budget successful!");
       setAmount(0);
       window.location = "/";
     } else {
-      throw new Error("Creating budget failed");
+      throw new Error("Updating budget failed");
     }
   };
 
