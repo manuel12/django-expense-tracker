fbf22acd32b05766d74dd4cc8000164842885bb8
Manuel <manuelpinedacabeza@gmail.com>
2023-07-26 17:38:41 +0200

    Updated tests to use data-test attributes as locators instead of ids.
    Added test to check user cannot signup with submitting
    username, password and password confirmation.

--- e2e/cypress/integration/ui-tests/signup.spec.js
+++ e2e/cypress/integration/ui-tests/signup.spec.js
@@ -14,49 +14,74 @@ describe("Signup Tests", () => {
     cy.visit("/");
   });
 
-  it("should show show homepage after signup", () => {
+  it("should show show homepage after signup and login", () => {
     cy.contains("Sign Up").click();
-    cy.get("#id_username")
+    cy.get("[data-test=username]")
       .type(username)
-      .get("#id_password1")
+      .get("[data-test=password]")
       .type(password)
-      .get("#id_password2")
+      .get("[data-test=password-confirmation]")
       .type(password);
     cy.get("[data-test=signup]").click();
 
-    cy.url().should("eq", Cypress.config().baseUrl);
+    cy.get("[data-test=success-message]").should(
+      "contain",
+      `User registered successfully!`
+    );
+
+    cy.url().should("contain", "/accounts/login");
+
+    cy.loginWithUI(username, password);
+  });
+
+  it("should NOT allow you to signup without submitting username, password and password confirmation", () => {
+    cy.contains("Sign Up").click();
+
+    cy.get("[data-test=signup]").click();
+
     cy.get("[data-test=user-greet]")
       .should("be.visible")
-      .and("contain", `Hi ${username}`)
-      .and("contain", "Log Out");
+      .and("contain", "You are not logged in")
+      .and("contain", "Sign Up")
+      .and("contain", "Log In");
+
+    cy.get("[data-test=container]").should(
+      "contain",
+      "You need to provide username, password and password confirmation."
+    );
   });
 
   it("should NOT allow you to signup with the exisiting username", () => {
     // Create testuser
     cy.contains("Sign Up").click();
-    cy.get("#id_username")
+    cy.get("[data-test=username]")
       .type(username)
-      .get("#id_password1")
+      .get("[data-test=password]")
       .type(password)
-      .get("#id_password2")
+      .get("[data-test=password-confirmation]")
       .type(password);
     cy.get("[data-test=signup]").click();
 
-    cy.logout();
+    cy.get("[data-test=success-message]").should(
+      "contain",
+      `User registered successfully!`
+    );
+
+    cy.url().should("contain", "/accounts/login");
 
     // Try to create same testuser again.
     cy.contains("Sign Up").click();
-    cy.get("#id_username")
+    cy.get("[data-test=username]")
       .type(username)
-      .get("#id_password1")
+      .get("[data-test=password]")
       .type(password)
-      .get("#id_password2")
+      .get("[data-test=password-confirmation]")
       .type(password);
     cy.get("[data-test=signup]").click();
 
-    cy.url().should("contain", "/accounts/signup/");
+    cy.url().should("contain", "/accounts/signup");
     cy.get("form").should("be.visible");
-    cy.get("[data-test=error-labels]").should(
+    cy.get("[data-test=user-taken-error-label]").should(
       "contain",
       "The username you entered has already been taken! Please try another username."
     );
@@ -67,17 +92,17 @@ describe("Signup Tests", () => {
     const password2 = "fake123pass";
 
     cy.contains("Sign Up").click();
-    cy.get("#id_username")
+    cy.get("[data-test=username]")
       .type(username)
-      .get("#id_password1")
+      .get("[data-test=password]")
       .type(password)
-      .get("#id_password2")
+      .get("[data-test=password-confirmation]")
       .type(password2);
     cy.get("[data-test=signup]").click();
 
-    cy.url().should("contain", "/accounts/signup/");
+    cy.url().should("contain", "/accounts/signup");
     cy.get("form").should("be.visible");
-    cy.get("[data-test=error-labels]").should(
+    cy.get("[data-test=passwords-no-match-error-label]").should(
       "contain",
       "The two password fields didn’t match!"
     );
