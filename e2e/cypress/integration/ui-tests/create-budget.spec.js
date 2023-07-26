70ce9dbd74d64b94520a94109213f60bbe24eca9
Manuel <manuelpinedacabeza@gmail.com>
2023-07-26 17:12:53 +0200

    Added setToken function.
    Removed deleteElementIfExists call and used
    loginAndCleanUp for that purpose.
    
    Commented out cookies preservation.
    
    Added cy.visit calls where neeeded.
    
    Updated should not allow more than 5 digits tests.

--- e2e/cypress/integration/ui-tests/create-budget.spec.js
+++ e2e/cypress/integration/ui-tests/create-budget.spec.js
@@ -7,14 +7,14 @@ const budgetData = require("../../fixtures/budget.json");
 describe("Create budget Tests", () => {
   const ctx = {};
 
-  before(() => {
-    cy.loginAndCleanUp();
-  });
+  const setTokens = (tokens) => {
+    ctx.access = tokens.access;
+    ctx.refresh = tokens.refresh;
+  };
 
   beforeEach(() => {
     // Delete budget to start clean.
-    cy.deleteElementIfExists("budget");
-
+    cy.loginAndCleanUp(setTokens);
     const budget = new Budget(budgetData);
     ctx.budget = budget;
 
@@ -23,13 +23,15 @@ describe("Create budget Tests", () => {
       ctx.createBudgetPageUrl = url;
     });
 
-    Cypress.Cookies.preserveOnce("sessionid");
+    // Cypress.Cookies.preserveOnce("sessionid");
   });
 
   it("should create a budget", () => {
+    cy.visit("/");
     cy.createBudgetWithUI(ctx.budget);
 
     cy.url().should("eq", Cypress.config().baseUrl);
+
     cy.get("[data-test=monthly-budget]")
       .should("be.visible")
       .and("contain", "Monthly budget:")
@@ -40,6 +42,8 @@ describe("Create budget Tests", () => {
   });
 
   it("should NOT allow to create a budget while leaving a 'amount' field on 0", () => {
+    cy.visit("/");
+
     ctx.budget.amount = 0;
     cy.createBudgetWithUI(ctx.budget);
 
@@ -55,14 +59,14 @@ describe("Create budget Tests", () => {
     cy.get("[data-test=create-budget-form]").should("be.visible");
   });
 
-  it("should NOT allow to create a budget with more than 5 digits in 'amount' number", () => {
-    ctx.budget.amount = 999999;
+  it("should NOT allow to create a budget higher than 999999", () => {
+    ctx.budget.amount = 1000000;
     cy.createBudgetWithUI(ctx.budget);
 
     cy.url().should("eq", ctx.createBudgetPageUrl);
-    cy.get("[data-test=create-budget-form]")
+    cy.get("[data-test=container]")
       .should("be.visible")
-      .and("contain", "Ensure that there are no more than 5 digits in total.");
+      .and("contain", "Ensure that budget is not higher than 999999.");
   });
 
   it("should remove 'Create Budget' button once the user has added a budget", () => {
