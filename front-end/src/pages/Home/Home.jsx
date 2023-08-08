e9d7fe7c3b7c3d54b24ca3da565bddf05ea55857
Manuel <manuelpinedacabeza@gmail.com>
2023-08-08 15:38:41 +0200

    Updated expenses to paginated expenses request.Uncommented Pagination.

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -22,10 +22,12 @@ const Home = ({ accessToken }) => {
   const [budget, setBudget] = useState(0);
   const [lineChartData, setLineChartData] = useState([]);
 
+  const token = accessToken;
+
   useEffect(() => {
     lineChartData.length === 0 &&
       API.fetchLineChartData(accessToken, setLineChartData);
-    expenses.length === 0 && API.fetchExpenses(accessToken, setExpenses);
+    expenses.length === 0 && API.fetchPaginatedExpenses({token, paginationSuffix: 1, setExpenses});
     !budget && API.fetchBudget(accessToken, setBudget);
   }, []);
 
@@ -57,7 +59,7 @@ const Home = ({ accessToken }) => {
 
       {expenses.length > 0 && <ExpenseTable expenses={expenses} />}
 
-      {/* <Pagination setExpenses={setExpenses}/> */}
+      <Pagination setExpenses={setExpenses}/>
     </>
   );
 };
