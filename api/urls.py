b86401b501aa7bacc6f68f833e0c39d9806ad790
Manuel <manuelpinedacabeza@gmail.com>
2023-08-09 20:32:38 +0200

    Formatted code

--- api/urls.py
+++ api/urls.py
@@ -10,18 +10,20 @@ urlpatterns = [
 
 
     path('expenses/', views.get_expenses, name='get_expenses'),
-    path('paginated-expenses/', views.get_paginated_expenses, name="get_paginated_expenses"),
+    path('paginated-expenses/', views.get_paginated_expenses,
+         name="get_paginated_expenses"),
     path('expenses/create/', views.create_expense, name='create_expense'),
     path('expenses/update/<int:pk>/', views.update_expense, name='update_expense'),
     path('expenses/delete/<int:pk>/', views.delete_expense, name='delete_expense'),
+
 
     path('budget/', views.get_budget, name='get_budget'),
     path('budget/create/', views.create_budget, name='create_budget'),
     path('budget/update/<int:pk>/', views.update_budget, name='update_budget'),
     path('budget/delete/<int:pk>/', views.delete_budget, name='delete_budget'),
 
-    path("line-chart-data/", views.line_chart_data, name="line_chart_data"),
 
+    path("line-chart-data/", views.line_chart_data, name="line_chart_data"),
     path(
         "expenses-by-month-bar-chart-data/",
         views.expenses_by_month_bar_chart_data,
@@ -48,9 +50,8 @@ urlpatterns = [
         name="statistics_table_data",
     ),
 
+
     path('add-testuser-data/', views.add_testuser_data, name='add_testuser_data'),
     path('delete-testuser-data/', views.delete_testuser_data,
          name='delete_testuser_data'),
-
-
 ]
