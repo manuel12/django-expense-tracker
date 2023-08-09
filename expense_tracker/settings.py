474689cc2331fc87c8fcd59b32381bcab2504cf5
Manuel <manuelpinedacabeza@gmail.com>
2023-08-09 20:17:54 +0200

    Commnted out db config section.

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -109,10 +109,10 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-DATABASE_URL = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
-DATABASES["default"] = dj_database_url.config(
-    default=DATABASE_URL, conn_max_age=600
-)
+# DATABASE_URL = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+# DATABASES["default"] = dj_database_url.config(
+#     default=DATABASE_URL, conn_max_age=600
+# )
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
